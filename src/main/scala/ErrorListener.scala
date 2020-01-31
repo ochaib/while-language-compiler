7b69b9608e48f045ade30c28e8acd16b07aea138
ochaib <amassosaa@gmail.com>
2020-01-31 18:00:36 +0000

    Added ErrorListener class with syntaxError method.

---
+++ src/main/scala/ErrorListener.scala
@@ -0,0 +1,13 @@
+package main.scala
+
+import org.antlr.v4.runtime.{BaseErrorListener, RecognitionException, Recognizer}
+
+class ErrorListener extends BaseErrorListener {
+
+  override def syntaxError(recognizer: Recognizer[_, _], offendingSymbol: AnyRef, line: Int,
+                           charPositionInLine: Int, msg: String, e: RecognitionException) {
+
+     println("Syntax Error on Line " + line + ":" + charPositionInLine + " " + msg)
+  }
+
+}
