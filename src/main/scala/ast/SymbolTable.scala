8237690705c7ed8d6bd6db9bf2f19fea018e587d
Daniel Loney <danieljloneyacc@gmail.com>
2020-02-06 12:57:19 +0000

    refactored SymbolTableFunctions into a companion object for SymbolTable

--- src/main/scala/ast/SymbolTable.scala
+++ src/main/scala/ast/SymbolTable.scala
@@ -1,5 +1,7 @@
 package ast
 
+import scala.collection.immutable.HashMap
+
 
 class SymbolTable(var map: Map[String, IDENTIFIER], var encSymbolTable: SymbolTable) {
 
@@ -30,4 +32,23 @@ class SymbolTable(var map: Map[String, IDENTIFIER], var encSymbolTable: SymbolTa
     }
     None
   }
+}
+
+object SymbolTable {
+  def topLevelSymbolTable(astTree: ASTNode): SymbolTable = {
+    val charKey = "char"
+    val boolKey = "bool"
+    val stringKey = "string"
+    val intKey = "int"
+    // return a symbol table with the map of all the standard types, global constants, global functions
+    val charScalar: TYPE = new SCALAR(charKey, min=0, max=255)
+    new SymbolTable(HashMap(intKey -> new SCALAR(intKey, min= Int.MinValue, max= Int.MaxValue),
+      charKey -> charScalar,
+      boolKey -> new SCALAR(boolKey, min=0, max=1),
+      stringKey -> new ARRAY(stringKey, charScalar),
+      GENERAL_PAIR.getKey -> GENERAL_PAIR))
+
+  }
+
+  def newSymbolTable(encSymbolTable: SymbolTable): SymbolTable = new SymbolTable(new HashMap(), encSymbolTable)
 }
--- src/main/scala/ast/Visitor.scala
+++
@@ -1,5 +0,0 @@
-package ast
-
-class Visitor {
-
-}
