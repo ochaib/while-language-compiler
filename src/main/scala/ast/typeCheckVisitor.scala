24c93eb664e0b9e4a8c1eff53cb10ab283a70324
Daniel Loney <danieljloneyacc@gmail.com>
2020-02-06 13:15:34 +0000

    implemented base for typeCheckVisitor

--- src/main/scala/ast/typeCheckVisitor.scala
+++ src/main/scala/ast/typeCheckVisitor.scala
@@ -1,5 +1,13 @@
 package ast
 
-class typeCheckVisitor {
+import scala.collection.immutable.HashMap
 
+sealed class typeCheckVisitor(entryNode: ASTNode) extends Visitor(entryNode) {
+  var topST: SymbolTable = SymbolTable.topLevelSymbolTable(entryNode)
+  var currentSymbolTable: SymbolTable = new SymbolTable(new HashMap(), topST)
+
+  override def visit(): Unit = entryNode match {
+
+  }
 }
+
