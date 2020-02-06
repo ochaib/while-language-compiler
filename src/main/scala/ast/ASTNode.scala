dde45ce2c596b864e492dc0abafe5be0178bef5d
ochaib <amassosaa@gmail.com>
2020-02-06 22:58:00 +0000

    Replaced calls using toString with getKey in ASTNode.scala.

--- src/main/scala/ast/ASTNode.scala
+++ src/main/scala/ast/ASTNode.scala
@@ -186,7 +186,7 @@ case class IdentNode(ident: String) extends ExprNode with AssignLHSNode {
   override def initKey: String = ident
 
   override def initType(topST: SymbolTable, ST: SymbolTable): TYPE = {
-    val T: Option[IDENTIFIER] = ST.lookupAll(toString)
+    val T: Option[IDENTIFIER] = ST.lookupAll(getKey)
     if (T.isEmpty) {
       throw new TypeException(s"$toString has not been declared")
     } else if (! T.get.isInstanceOf[VARIABLE]) {
@@ -214,7 +214,7 @@ case class ArrayLiteralNode(exprNodes: IndexedSeq[ExprNode]) extends AssignRHSNo
     val arrayIdentifierOption: Option[IDENTIFIER] = topST.lookup(getKey)
     if (arrayIdentifierOption.isEmpty) {
       val arrayIdentifier = new ARRAY(getKey, exprNodes.apply(0).getType(topST, ST).asInstanceOf[TYPE])
-      topST.add(toString, arrayIdentifier)
+      topST.add(getKey, arrayIdentifier)
       arrayIdentifier
     } else {
       assert(arrayIdentifierOption.get.isInstanceOf[ARRAY], s"Something went wrong... $getKey should be a an array type but isn't")
