7b23a7c79520cea6f2cfbeac7e498f7132a6f0ab
ochaib <amassosaa@gmail.com>
2020-02-06 20:57:39 +0000

    Converted error logger to queue based instead of stack based.

--- src/main/scala/util/ErrorLog.scala
+++ src/main/scala/util/ErrorLog.scala
@@ -6,16 +6,16 @@ abstract class ErrorLog {
 
   val errorType: String
   var errorCheck: Boolean = false
-  val errorLog: mutable.Stack[String] = new mutable.Stack[String]()
+  val errorLog: mutable.Queue[String] = new mutable.Queue[String]()
 
   def add(string: String): Unit = {
-    errorLog.push(string)
+    errorLog += string
     errorCheck = true
   }
 
   def printAllErrors(): Unit = {
     while (errorLog.nonEmpty) {
-      println(errorType + errorLog.pop())
+      println(errorType + errorLog.dequeue)
     }
   }
 }
