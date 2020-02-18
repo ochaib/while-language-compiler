6e9fb5cbeb9d1215ace317aacd3ece3fde5f92e4
Priansh Shah <shah@priansh.com>
2020-02-18 09:05:21 +0000

    add R0 and define more of the arm 11 instruction set

--- src/main/scala/asm/instructionset/ARM11.scala
+++ src/main/scala/asm/instructionset/ARM11.scala
@@ -5,6 +5,7 @@ import asm.registers.Register
 import scala.collection.mutable.ListBuffer
 
 sealed trait ARM11Register extends Register
+case object R0 extends ARM11Register {val registerID = "R0"}
 case object R1 extends ARM11Register {val registerID = "R1"}
 case object R2 extends ARM11Register {val registerID = "R2"}
 case object R3 extends ARM11Register {val registerID = "R3"}
@@ -19,7 +20,7 @@ case object R11 extends ARM11Register {val registerID = "R11"}
 case object R12 extends ARM11Register {val registerID = "R12"}
 // Stack pointer (R13)
 case object SP extends ARM11Register {val registerID = "SP"}
-// Load register (R14)
+// Link register (R14)
 case object LR extends ARM11Register {val registerID = "LR"}
 // Program counter (R15)
 case object PC extends ARM11Register {val registerID = "PC"}
@@ -29,23 +30,28 @@ object ARM11 extends InstructionSet {
 
   // Particular register set for the instruction.
   val registers: ListBuffer[Register] = ListBuffer[Register](
-    R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12)
+    R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, SP, LR, PC)
 
   // Return registers that are saved by the function e.g. r4-r11, r13
-  def getCalleeSaved: ListBuffer[Register] = ???
+  def getCalleeSaved: ListBuffer[Register] = ListBuffer[Register](
+    R4, R5, R6, R7, R8, R9, R10, R11, // registers for local variables
+    SP // stack pointer
+  )
   // Return registers that are saved by the thing that calls the function e.g. r0-r3, r12
-  def getCallerSaved: ListBuffer[Register] = ???
+  def getCallerSaved: ListBuffer[Register] = R12 +: getArgumentRegisters
 
   // Return stack pointer.
-  def getSP: Register = ???
+  def getSP: Register = SP
   // Return program counter.
-  def getPC: Register = ???
+  def getPC: Register = PC
   // Return the link register.
-  def getLR: Register = ???
+  def getLR: Register = LR
   // Return the return registers, r0 or r1.
-  def getReturn: Register = ???
+  def getReturn: Register = R0 // by convention
   // Return the argument registers, ro-r3.
-  def getArgumentRegisters: ListBuffer[Register] = ???
+  def getArgumentRegisters: ListBuffer[Register] = ListBuffer[Register](
+    R0, R1, R2, R3
+  )
   // Return the variable? registers, possibly not necessary.
   def getVariableRegisters: ListBuffer[Register] = ???
 
