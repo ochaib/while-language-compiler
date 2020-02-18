9994795f1cebff0d82ab46d0268960ea2bceced8
Priansh Shah <shah@priansh.com>
2020-02-18 06:45:44 +0000

    add base generator

---
+++ src/main/scala/asm/generator/CodeGenerator.scala
@@ -0,0 +1,11 @@
+package asm.generator
+
+import asm.instructions._
+import ast.nodes._
+
+object CodeGenerator extends Generator {
+    def generate(program: Any): IndexedSeq[Any] = {
+        // TODO
+        new IndexedSeq[Instruction]
+    }
+}
