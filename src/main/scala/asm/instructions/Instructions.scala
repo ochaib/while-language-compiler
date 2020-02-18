ad66126fbcfe2da28dc55a8d92342ee9fcf8c30b
dl4317 <dl4317@ic.ac.uk>
2020-02-18 14:59:58 +0000

    refactored conditions to be optional

--- src/main/scala/asm/instructions/Instructions.scala
+++ src/main/scala/asm/instructions/Instructions.scala
@@ -3,11 +3,11 @@ package asm.instructions
 import asm.registers.Register
 
 // could condition be an Option here instead of having an Any condition?
-sealed abstract class Instruction(condition: Condition)
+abstract class Instruction(condition: Option[Condition])
 
-case class Push(condition: Condition, registers: List[Register])
+case class Push(condition: Option[Condition], registers: List[Register])
     extends Instruction(condition)
-case class Pop(condition: Condition, registers: List[Register])
+case class Pop(condition: Option[Condition], registers: List[Register])
     extends Instruction(condition)
 
 // NOTE: a case class can't inherit a case class
@@ -15,36 +15,37 @@ case class Pop(condition: Condition, registers: List[Register])
 // these non-leaf classes can't be pattern matched on
 // alternatively, we could turn these into traits
 case class LoadDirect private(
-                               condition: Condition,
+                               condition: Option[Condition],
                                byteType: Boolean,
                                _type: Option[WordType],
                                dest: Register,
                                src: Option[Register],
                                offset: Option[FlexOffset],
-                               registerWriteBack: Boolean, // the "!"
+                               registerWriteBack: Option[Boolean], // the "!"
                                loadable: Option[Loadable],
                                label: Option[Label]
 ) extends Instruction(condition) {
   assert(!(byteType && _type.isDefined), "Can't simultaneously be a byteType and another type")
+  assert(label.isDefined || src.isDefined, "Either a label or source must be defined")
   // LDR{cond}{B|Type} Rd, [Rn]
-  def this(condition: Condition, byteType: Boolean = false, _type: WordType, dest: Register, src: Register) =
-    this(condition, byteType, Some(_type), dest, Some(src), None, registerWriteBack = false, None, None)
+  def this(condition: Option[Condition], byteType: Boolean = false, _type: WordType, dest: Register, src: Register) =
+    this(condition, byteType, Some(_type), dest, Some(src), None, None, None, None)
   // LDR{cond}{B|Type} Rd, [Rn, FlexOffset]{!}
-  def this(condition: Condition, byteType: Boolean = false, _type: WordType, dest: Register, src: Register, flexOffset: FlexOffset, registerWriteBack: Boolean = false) =
-    this(condition, byteType, Some(_type), dest, Some(src), Some(flexOffset), registerWriteBack, None, None)
+  def this(condition: Option[Condition], byteType: Boolean = false, _type: WordType, dest: Register, src: Register, flexOffset: FlexOffset, registerWriteBack: Boolean = false) =
+    this(condition, byteType, Some(_type), dest, Some(src), Some(flexOffset), Some(registerWriteBack), None, None)
   // LDR{cond}{B|Type} Rd, label
-  def this(condition: Condition, byteType: Boolean = false, _type: WordType, dest: Register, label: Label) =
-    this(condition, byteType, Some(_type), dest, None, None, registerWriteBack = false, None, Some(label))
+  def this(condition: Option[Condition], byteType: Boolean = false, _type: WordType, dest: Register, label: Label) =
+    this(condition, byteType, Some(_type), dest, None, None, None, None, Some(label))
   // LDR{cond}{B|Type} Rd, [Rn], FlexOffset
-  def this(condition: Condition, byteType: Boolean = false, _type: WordType, dest: Register, src: Register, offset: FlexOffset) =
-    this(condition, byteType, Some(_type), dest, Some(src), Some(offset), registerWriteBack = false, None, None)
+  def this(condition: Option[Condition], byteType: Boolean = false, _type: WordType, dest: Register, src: Register, offset: FlexOffset) =
+    this(condition, byteType, Some(_type), dest, Some(src), Some(offset), None, None, None)
   // LDR{cond}{B|Type} register, =[expr | label-expr]
-  def this(condition: Condition, byteType: Boolean = false, _type: WordType, dest: Register, loadable: Loadable) =
-    this(condition, byteType, Some(_type), dest, None, None, registerWriteBack = false, Some(loadable), null)
+  def this(condition: Option[Condition], byteType: Boolean = false, _type: WordType, dest: Register, loadable: Loadable) =
+    this(condition, byteType, Some(_type), dest, None, None, None, Some(loadable), null)
 }
 
 case class Store private(
-                          condition: Condition,
+                          condition: Option[Condition],
                           byteType: Boolean,
                           dest: Register,
                           src: Option[Register],
@@ -52,30 +53,31 @@ case class Store private(
                           registerWriteBack: Boolean, // the "!"
                           label: Option[Label]
                         ) extends Instruction(condition) {
+  assert(label.isDefined || src.isDefined, "Either a label or source must be defined")
   // STR{cond}{B} Rd, [Rn]
-  def this(condition: Condition, byteType: Boolean = false, dest: Register, src: Register) =
+  def this(condition: Option[Condition], byteType: Boolean = false, dest: Register, src: Register) =
     this(condition, byteType, dest, Some(src), None, registerWriteBack = false, None)
   // STR{cond}{B} Rd, [Rn, FlexOffset]{!}
-  def this(condition: Condition, byteType: Boolean = false, dest: Register, src: Register, flexOffset: FlexOffset, registerWriteBack: Boolean) =
+  def this(condition: Option[Condition], byteType: Boolean = false, dest: Register, src: Register, flexOffset: FlexOffset, registerWriteBack: Boolean) =
     this(condition, byteType, dest, Some(src), Some(flexOffset), registerWriteBack, None)
   // STR{cond}{B} Rd, label
-  def this(condition: Condition, byteType: Boolean = false, dest: Register, label: Label) =
+  def this(condition: Option[Condition], byteType: Boolean = false, dest: Register, label: Label) =
     this(condition, byteType, dest, None, None, registerWriteBack = false, Some(label))
   // STR{cond}{B} Rd, [Rn], FlexOffset
-  def this(condition: Condition, byteType: Boolean = false, dest: Register, src: Register, offset: FlexOffset) =
+  def this(condition: Option[Condition], byteType: Boolean = false, dest: Register, src: Register, offset: FlexOffset) =
     this(condition, byteType, dest, Some(src), Some(offset), registerWriteBack = false, None)
 }
 
 // Data Process Instructions include ADD, SUB, ORR, EOR
 sealed abstract class DataProcess(
-    condition: Condition,
+    condition: Option[Condition],
     conditionFlag: Boolean,
     dest: Register,
     src1: Register,
     src2: FlexibleSndOp
-)
+) extends Instruction(condition)
 case class Add(
-    condition: Condition,
+    condition: Option[Condition],
     conditionFlag: Boolean,
     dest: Register,
     src1: Register,
@@ -83,7 +85,7 @@ case class Add(
 ) extends DataProcess(condition, conditionFlag, dest, src1, src2)
 
 case class Subtract(
-    condition: Condition,
+    condition: Option[Condition],
     conditionFlag: Boolean,
     dest: Register,
     src1: Register,
@@ -91,7 +93,7 @@ case class Subtract(
 ) extends DataProcess(condition, conditionFlag, dest, src1, src2)
 
 case class And(
-    condition: Condition,
+    condition: Option[Condition],
     conditionFlag: Boolean,
     dest: Register,
     src1: Register,
@@ -99,7 +101,7 @@ case class And(
 ) extends DataProcess(condition, conditionFlag, dest, src1, src2)
 
 case class Or(
-    condition: Condition,
+    condition: Option[Condition],
     conditionFlag: Boolean,
     dest: Register,
     src1: Register,
@@ -107,21 +109,21 @@ case class Or(
 ) extends DataProcess(condition, conditionFlag, dest, src1, src2)
 
 case class ExclusiveOr(
-    condition: Condition,
+    condition: Option[Condition],
     conditionFlag: Boolean,
     dest: Register,
     src1: Register,
     src2: FlexibleSndOp
 ) extends DataProcess(condition, conditionFlag, dest, src1, src2)
 
-case class Move(condition: Condition, dest: Register, src: FlexibleSndOp
+case class Move(condition: Option[Condition], dest: Register, src: FlexibleSndOp
                ) extends Instruction(condition)
 
-case class Compare(condition: Condition, operand1: Register, operand2: FlexibleSndOp
+case class Compare(condition: Option[Condition], operand1: Register, operand2: FlexibleSndOp
                   ) extends Instruction(condition)
 
-case class Branch(condition: Condition, label: Label
+case class Branch(condition: Option[Condition], label: Label
                  ) extends Instruction(condition)
 
-case class BranchLabel(condition: Condition, label: Label
+case class BranchLabel(condition: Option[Condition], label: Label
                       ) extends Instruction(condition)
