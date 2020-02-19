97e4d26c67188cadf88acf6397f9f03f11c47806
Priansh Shah <shah@priansh.com>
2020-02-19 12:41:07 +0000

    lowercase register names

--- src/main/scala/asm/instructionset/ARM11.scala
+++ src/main/scala/asm/instructionset/ARM11.scala
@@ -6,25 +6,25 @@ import asm.registers.Register
 import scala.collection.mutable.ListBuffer
 
 sealed trait ARM11Register extends Register
-case object R0 extends ARM11Register { val registerID = "R0" }
-case object R1 extends ARM11Register { val registerID = "R1" }
-case object R2 extends ARM11Register { val registerID = "R2" }
-case object R3 extends ARM11Register { val registerID = "R3" }
-case object R4 extends ARM11Register { val registerID = "R4" }
-case object R5 extends ARM11Register { val registerID = "R5" }
-case object R6 extends ARM11Register { val registerID = "R6" }
-case object R7 extends ARM11Register { val registerID = "R7" }
-case object R8 extends ARM11Register { val registerID = "R8" }
-case object R9 extends ARM11Register { val registerID = "R9" }
-case object R10 extends ARM11Register { val registerID = "R10" }
-case object R11 extends ARM11Register { val registerID = "R11" }
-case object R12 extends ARM11Register { val registerID = "R12" }
+case object R0 extends ARM11Register { val registerID = "r0" }
+case object R1 extends ARM11Register { val registerID = "r1" }
+case object R2 extends ARM11Register { val registerID = "r2" }
+case object R3 extends ARM11Register { val registerID = "r3" }
+case object R4 extends ARM11Register { val registerID = "r4" }
+case object R5 extends ARM11Register { val registerID = "r5" }
+case object R6 extends ARM11Register { val registerID = "r6" }
+case object R7 extends ARM11Register { val registerID = "r7" }
+case object R8 extends ARM11Register { val registerID = "r8" }
+case object R9 extends ARM11Register { val registerID = "r9" }
+case object R10 extends ARM11Register { val registerID = "r10" }
+case object R11 extends ARM11Register { val registerID = "r11" }
+case object R12 extends ARM11Register { val registerID = "r12" }
 // Stack pointer (R13)
-case object SP extends ARM11Register { val registerID = "SP" }
+case object SP extends ARM11Register { val registerID = "sp" }
 // Link register (R14)
-case object LR extends ARM11Register { val registerID = "LR" }
+case object LR extends ARM11Register { val registerID = "lr" }
 // Program counter (R15)
-case object PC extends ARM11Register { val registerID = "PC" }
+case object PC extends ARM11Register { val registerID = "pc" }
 
 // TODO: implement
 object ARM11 extends InstructionSet {
