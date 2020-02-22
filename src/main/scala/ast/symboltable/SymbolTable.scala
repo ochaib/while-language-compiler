8b7e6da1734378a02fa97cb8d835e0dd7eedc25e
Daniel Loney <danieljloneyacc@gmail.com>
2020-02-22 19:40:21 +0000

    implemented String identifiers

--- src/main/scala/ast/symboltable/SymbolTable.scala
+++ src/main/scala/ast/symboltable/SymbolTable.scala
@@ -61,12 +61,11 @@ class SymbolTable(var map: Map[String, IDENTIFIER], var funcMap: Map[String, FUN
 object SymbolTable {
   def topLevelSymbolTable(astTree: ASTNode): SymbolTable = {
     // return a symbol table with the map of all the standard types, global constants, global functions
-    val charScalar: TYPE = new SCALAR(new CharTypeNode(null).getKey, min=0, max=255)
     // TODO: refactor
     new SymbolTable(HashMap(new IntTypeNode(null).getKey -> new SCALAR(new IntTypeNode(null).getKey, min= Int.MinValue, max= Int.MaxValue),
-      new CharTypeNode(null).getKey -> charScalar,
+      new CharTypeNode(null).getKey -> new SCALAR(new CharTypeNode(null).getKey, min=0, max=255),
       new BoolTypeNode(null).getKey -> new SCALAR(new BoolTypeNode(null).getKey, min=0, max=1),
-      new StringTypeNode(null).getKey -> new ARRAY(new StringTypeNode(null).getKey, charScalar),
+      STRING.getKey -> STRING,
       GENERAL_PAIR.getKey -> GENERAL_PAIR,
       GENERAL_ARRAY.getKey -> GENERAL_ARRAY), new HashMap())
 
