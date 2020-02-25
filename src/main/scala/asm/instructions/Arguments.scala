8f0dd27247549dfbf24d2473af86b914ca502141
Priansh Shah <shah@priansh.com>
2020-02-25 18:06:15 +0000

    add string capability to immediates

--- src/main/scala/asm/instructions/Arguments.scala
+++ src/main/scala/asm/instructions/Arguments.scala
@@ -6,13 +6,20 @@ trait FlexOffset
 trait FlexibleSndOp
 trait Loadable
 
-class Immediate private(val immediate_int: Option[Int], val immediate_char: Option[Char]) extends FlexibleSndOp with FlexOffset {
-  def this(immediate_int: Int) = this(Some(immediate_int), None)
-  def this(immediate_char: Char) = this(None, Some(immediate_char))
+// TODO: this should take an abstract type parameter T instead of hard-coding every type
+class Immediate private(
+  val immediate_int: Option[Int],
+  val immediate_char: Option[Char],
+  val immediate_str: Option[String]
+) extends FlexibleSndOp with FlexOffset {
+  def this(immediate_int: Int) = this(Some(immediate_int), None, None)
+  def this(immediate_char: Char) = this(None, Some(immediate_char), None)
+  def this(immediate_str: String) = this(None, None, Some(immediate_str))
   override def toString: String = {
-    assert(immediate_int.isDefined || immediate_char.isDefined, "Immediate must have a value")
+    assert(immediate_int.isDefined || immediate_char.isDefined || immediate_str.isDefined, "Immediate must have a value")
     if (immediate_char.isDefined) return s"'${immediate_char.get.toString}'"
     if (immediate_int.isDefined) return immediate_int.get.toString
+    if (immediate_str.isDefined) return immediate_str.get
     throw new NoSuchFieldError
   }
 }
