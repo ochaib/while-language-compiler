92c737348f6962fd1d1ac292d9d1e45143486614
Priansh Shah <shah@priansh.com>
2020-02-27 18:12:35 +0000

    add exit and print check array bounds

--- src/main/scala/asm/utilities/Common.scala
+++ src/main/scala/asm/utilities/Common.scala
@@ -11,12 +11,15 @@ case object PrintBool extends CommonFunction { val label: Label = new Label("p_p
 case object PrintInt extends CommonFunction { val label: Label = new Label("p_print_int") }
 case object PrintOverflowError extends CommonFunction { val label: Label = new Label("p_throw_overflow_error") }
 case object PrintRuntimeError extends CommonFunction { val label: Label = new Label("p_throw_runtime_error") }
+case object PrintCheckArrayBounds extends CommonFunction { val label: Label = new Label("p_check_array_bounds") }
 
 // NOTE: the functions below DO NOT need to be generated
 // they come with the assembler (at least in ARM 11)
+// TODO: replace existing uses of these with the object
 abstract class StandardFunction { val label: Label }
 object PutChar { val label: Label = new Label("putchar") }
 object Puts { val label: Label = new Label("puts") }
 object Flush { val label: Label = new Label("fflush") }
 object Printf { val label: Label = new Label("printf") }
 object Malloc { val label: Label = new Label("malloc") }
+object Exit { val label: Label = new Label("exit") }
