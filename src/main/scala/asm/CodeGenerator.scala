fed528ba3fccbf9b4a238b7b5b349ac35318ceaf
ochaib <amassosaa@gmail.com>
2020-02-28 15:07:24 +0000

    Fixed register allocation for generating expressions.

--- src/main/scala/asm/CodeGenerator.scala
+++ src/main/scala/asm/CodeGenerator.scala
@@ -898,30 +898,34 @@ object CodeGenerator {
   def generateBinary(binaryOperation: BinaryOperationNode): IndexedSeq[Instruction] = {
     val varReg1 = RM.nextVariableRegister()
     val varReg2 = RM.peekVariableRegister()
-    RM.freeVariableRegister(varReg1)
     val r1 = instructionSet.getArgumentRegisters(1)
+    RM.freeVariableRegister(varReg1)
 
-    binaryOperation match {
+    // Necessary to insure register correctness
+    var firstArgument: ExprNode = Int_literNode(null, null)
+    var secondArgument: ExprNode = Int_literNode(null, null)
+
+    val binOpInstructions: IndexedSeq[Instruction] = binaryOperation match {
       case MultiplyNode(_, argOne, argTwo) =>
         generateExpression(argOne) ++ generateExpression(argTwo) ++
-        IndexedSeq[Instruction](
-          SMull(None, conditionFlag = false, varReg1, varReg2, varReg1, varReg2),
-          // Need to be shifted by 31, ASR 31
-          Compare(None, varReg2, new ShiftedRegister(varReg1, "ASR", 31))) ++ Utilities.printOverflowError(Some(NotEqual))
+          IndexedSeq[Instruction](
+            SMull(None, conditionFlag = false, varReg1, varReg2, varReg1, varReg2),
+            // Need to be shifted by 31, ASR 31
+            Compare(None, varReg2, new ShiftedRegister(varReg1, "ASR", 31))) ++ Utilities.printOverflowError(Some(NotEqual))
       case DivideNode(_, argOne, argTwo) =>
         generateExpression(argOne) ++ generateExpression(argTwo) ++
           IndexedSeq[Instruction](
             Move(None, instructionSet.getReturn, new ShiftedRegister(varReg1)),
             Move(None, r1, new ShiftedRegister(varReg2))
           ) ++ Utilities.printDivideByZero ++ IndexedSeq[Instruction](
-            BranchLink(None, DivMod.label),
-            Move(None, varReg1, new ShiftedRegister(instructionSet.getReturn)))
+          BranchLink(None, DivMod.label),
+          Move(None, varReg1, new ShiftedRegister(instructionSet.getReturn)))
       case ModNode(_, argOne, argTwo) =>
         generateExpression(argOne) ++ generateExpression(argTwo) ++
-        IndexedSeq[Instruction](
-          Move(None, instructionSet.getReturn, new ShiftedRegister(varReg1)),
-          Move(None, r1, new ShiftedRegister(varReg2))
-        ) ++ Utilities.printDivideByZero ++ IndexedSeq[Instruction](
+          IndexedSeq[Instruction](
+            Move(None, instructionSet.getReturn, new ShiftedRegister(varReg1)),
+            Move(None, r1, new ShiftedRegister(varReg2))
+          ) ++ Utilities.printDivideByZero ++ IndexedSeq[Instruction](
           BranchLink(None, DivMod.label),
           Move(None, varReg1, new ShiftedRegister(r1)))
       case PlusNode(_, argOne, argTwo) =>
@@ -932,55 +936,69 @@ object CodeGenerator {
           ) ++ Utilities.printOverflowError(Some(Overflow))
       case MinusNode(_, argOne, argTwo) =>
         generateExpression(argOne) ++ generateExpression(argTwo) ++
-        IndexedSeq[Instruction](
-          Subtract(None, conditionFlag = true, varReg1, varReg1, new ShiftedRegister(varReg2))
-        ) ++ Utilities.printOverflowError(Some(Overflow))
+          IndexedSeq[Instruction](
+            Subtract(None, conditionFlag = true, varReg1, varReg1, new ShiftedRegister(varReg2))
+          ) ++ Utilities.printOverflowError(Some(Overflow))
       case GreaterThanNode(_, argOne, argTwo) =>
         generateExpression(argOne) ++ generateExpression(argTwo) ++
-        IndexedSeq[Instruction](
-          Compare(None, varReg1, new ShiftedRegister(varReg2)),
-          Move(Some(GreaterThan), varReg1, new Immediate(1)),
-          Move(Some(LessEqual), varReg1, new Immediate(0)))
+          IndexedSeq[Instruction](
+            Compare(None, varReg1, new ShiftedRegister(varReg2)),
+            Move(Some(GreaterThan), varReg1, new Immediate(1)),
+            Move(Some(LessEqual), varReg1, new Immediate(0)))
       case GreaterEqualNode(_, argOne, argTwo) =>
         generateExpression(argOne) ++ generateExpression(argTwo) ++
-        IndexedSeq[Instruction](
-          Compare(None, varReg1, new ShiftedRegister(varReg2)),
-          Move(Some(GreaterEqual), varReg1, new Immediate(1)),
-          Move(Some(LessThan), varReg1, new Immediate(0)))
+          IndexedSeq[Instruction](
+            Compare(None, varReg1, new ShiftedRegister(varReg2)),
+            Move(Some(GreaterEqual), varReg1, new Immediate(1)),
+            Move(Some(LessThan), varReg1, new Immediate(0)))
       case LessThanNode(_, argOne, argTwo) =>
         generateExpression(argOne) ++ generateExpression(argTwo) ++
-        IndexedSeq[Instruction](
-          Compare(None, varReg1, new ShiftedRegister(varReg2)),
-          Move(Some(LessThan), varReg1, new Immediate(1)),
-          Move(Some(GreaterEqual), varReg1, new Immediate(0)))
+          IndexedSeq[Instruction](
+            Compare(None, varReg1, new ShiftedRegister(varReg2)),
+            Move(Some(LessThan), varReg1, new Immediate(1)),
+            Move(Some(GreaterEqual), varReg1, new Immediate(0)))
       case LessEqualNode(_, argOne, argTwo) =>
         generateExpression(argOne) ++ generateExpression(argTwo) ++
-        IndexedSeq[Instruction](
-          Compare(None, varReg1, new ShiftedRegister(varReg2)),
-          Move(Some(LessEqual), varReg1, new Immediate(1)),
-          Move(Some(GreaterThan), varReg1, new Immediate(0)))
+          IndexedSeq[Instruction](
+            Compare(None, varReg1, new ShiftedRegister(varReg2)),
+            Move(Some(LessEqual), varReg1, new Immediate(1)),
+            Move(Some(GreaterThan), varReg1, new Immediate(0)))
       case EqualToNode(_, argOne, argTwo) =>
-        generateExpression(argOne) ++ generateExpression(argTwo) ++
+        firstArgument = argOne
+        secondArgument = argTwo
         IndexedSeq[Instruction](
-          Compare(None, varReg1, new ShiftedRegister(varReg2)),
-          Move(Some(Equal), varReg1, new Immediate(1)),
-          Move(Some(NotEqual), varReg1, new Immediate(0)))
+            Compare(None, varReg1, new ShiftedRegister(varReg2)),
+            Move(Some(Equal), varReg1, new Immediate(1)),
+            Move(Some(NotEqual), varReg1, new Immediate(0)))
       case NotEqualNode(_, argOne, argTwo) =>
         generateExpression(argOne) ++ generateExpression(argTwo) ++
-        IndexedSeq[Instruction](
-          Compare(None, varReg1, new ShiftedRegister(varReg2)),
-          Move(Some(NotEqual), varReg1, new Immediate(1)),
-          Move(Some(Equal), varReg1, new Immediate(0)))
+          IndexedSeq[Instruction](
+            Compare(None, varReg1, new ShiftedRegister(varReg2)),
+            Move(Some(NotEqual), varReg1, new Immediate(1)),
+            Move(Some(Equal), varReg1, new Immediate(0)))
 
       case LogicalAndNode(_, argOne, argTwo) =>
         generateExpression(argOne) ++ generateExpression(argTwo) ++
-        IndexedSeq[Instruction](And(None, conditionFlag = false, varReg1,
-                                    varReg1, new ShiftedRegister(varReg2)))
+          IndexedSeq[Instruction](And(None, conditionFlag = false, varReg1,
+            varReg1, new ShiftedRegister(varReg2)))
       case LogicalOrNode(_, argOne, argTwo) =>
         generateExpression(argOne) ++ generateExpression(argTwo) ++
-        IndexedSeq[Instruction](Or(None, conditionFlag = false, varReg1,
-                                   varReg1, new ShiftedRegister(varReg2)))
+          IndexedSeq[Instruction](Or(None, conditionFlag = false, varReg1,
+            varReg1, new ShiftedRegister(varReg2)))
     }
+
+    // Generate the instructions for the first argument.
+    val firstArgInstructions: IndexedSeq[Instruction] = generateExpression(firstArgument)
+
+    // Call next register as previous variable register is storing first arg instructions.
+    val varReg3: Register = RM.nextVariableRegister()
+    // Generate the instructions for the second argument.
+    val secondArgInstructions: IndexedSeq[Instruction] = generateExpression(secondArgument)
+    // Free register used by generateExpression to allow other generators to use it.
+    RM.freeVariableRegister(varReg3)
+
+    // Return all instructions in the correct order.
+    firstArgInstructions ++ secondArgInstructions ++ binOpInstructions
   }
 
   def generateCommonFunction(func: CommonFunction): IndexedSeq[Instruction] = func match {
